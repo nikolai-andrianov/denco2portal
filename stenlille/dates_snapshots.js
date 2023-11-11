7a0499877c74dd8ed51eebb5e518c90d24d7ace0
nikolai-andrianov <n_andrianov@hotmail.com>
2023-11-11 08:24:40 +0100

    Stenlille data

---
+++ stenlille/dates_snapshots.js
@@ -0,0 +1,162 @@
+const dates_snapshots = [
+    {
+        "date": "2023-01-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00000.svg"
+    },
+    {
+        "date": "2023-03-02",
+        "snapshot": "./i6Wp6W_BHP/Sg_00001.svg"
+    },
+    {
+        "date": "2023-04-02",
+        "snapshot": "./i6Wp6W_BHP/Sg_00002.svg"
+    },
+    {
+        "date": "2023-05-02",
+        "snapshot": "./i6Wp6W_BHP/Sg_00003.svg"
+    },
+    {
+        "date": "2023-06-02",
+        "snapshot": "./i6Wp6W_BHP/Sg_00004.svg"
+    },
+    {
+        "date": "2023-07-02",
+        "snapshot": "./i6Wp6W_BHP/Sg_00005.svg"
+    },
+    {
+        "date": "2023-08-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00006.svg"
+    },
+    {
+        "date": "2023-09-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00007.svg"
+    },
+    {
+        "date": "2023-10-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00008.svg"
+    },
+    {
+        "date": "2023-11-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00009.svg"
+    },
+    {
+        "date": "2023-12-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00010.svg"
+    },
+    {
+        "date": "2023-12-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00011.svg"
+    },
+    {
+        "date": "2024-01-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00012.svg"
+    },
+    {
+        "date": "2024-03-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00013.svg"
+    },
+    {
+        "date": "2024-04-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00014.svg"
+    },
+    {
+        "date": "2024-05-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00015.svg"
+    },
+    {
+        "date": "2024-05-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00016.svg"
+    },
+    {
+        "date": "2024-07-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00017.svg"
+    },
+    {
+        "date": "2024-07-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00018.svg"
+    },
+    {
+        "date": "2024-08-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00019.svg"
+    },
+    {
+        "date": "2024-09-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00020.svg"
+    },
+    {
+        "date": "2024-10-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00021.svg"
+    },
+    {
+        "date": "2024-11-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00022.svg"
+    },
+    {
+        "date": "2024-12-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00023.svg"
+    },
+    {
+        "date": "2025-01-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00024.svg"
+    },
+    {
+        "date": "2025-03-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00025.svg"
+    },
+    {
+        "date": "2025-04-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00026.svg"
+    },
+    {
+        "date": "2025-05-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00027.svg"
+    },
+    {
+        "date": "2025-05-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00028.svg"
+    },
+    {
+        "date": "2025-07-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00029.svg"
+    },
+    {
+        "date": "2025-07-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00030.svg"
+    },
+    {
+        "date": "2025-08-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00031.svg"
+    },
+    {
+        "date": "2025-09-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00032.svg"
+    },
+    {
+        "date": "2025-10-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00033.svg"
+    },
+    {
+        "date": "2025-11-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00034.svg"
+    },
+    {
+        "date": "2025-12-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00035.svg"
+    },
+    {
+        "date": "2026-01-30",
+        "snapshot": "./i6Wp6W_BHP/Sg_00036.svg"
+    },
+    {
+        "date": "2026-03-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00037.svg"
+    },
+    {
+        "date": "2026-03-31",
+        "snapshot": "./i6Wp6W_BHP/Sg_00038.svg"
+    },
+    {
+        "date": "2026-05-01",
+        "snapshot": "./i6Wp6W_BHP/Sg_00039.svg"
+    }
+] 
---
+++ stenlille/stenlille_wells.js
@@ -0,0 +1,102 @@
+const wells = [
+    {
+        "Name": "ST-1",
+        "N_Degrees": 55.54395556,
+        "E_Degrees": 11.61844722
+    },
+    {
+        "Name": "ST-10",
+        "N_Degrees": 55.55003611,
+        "E_Degrees": 11.59965833
+    },
+    {
+        "Name": "ST-11",
+        "N_Degrees": 55.53861389,
+        "E_Degrees": 11.60666944
+    },
+    {
+        "Name": "ST-12",
+        "N_Degrees": 55.53841389,
+        "E_Degrees": 11.60615556
+    },
+    {
+        "Name": "ST-13",
+        "N_Degrees": 55.53834444,
+        "E_Degrees": 11.60556667
+    },
+    {
+        "Name": "ST-14",
+        "N_Degrees": 55.53868056,
+        "E_Degrees": 11.60623333
+    },
+    {
+        "Name": "ST-15",
+        "N_Degrees": 55.51160556,
+        "E_Degrees": 11.56556944
+    },
+    {
+        "Name": "ST-16",
+        "N_Degrees": 55.538725,
+        "E_Degrees": 11.60576389
+    },
+    {
+        "Name": "ST-17",
+        "N_Degrees": 55.53386944,
+        "E_Degrees": 11.59887778
+    },
+    {
+        "Name": "ST-18",
+        "N_Degrees": 55.53397222,
+        "E_Degrees": 11.59843889
+    },
+    {
+        "Name": "ST-19",
+        "N_Degrees": 55.53361944,
+        "E_Degrees": 11.59869444
+    },
+    {
+        "Name": "ST-2",
+        "N_Degrees": 55.53814722,
+        "E_Degrees": 11.61088056
+    },
+    {
+        "Name": "ST-20",
+        "N_Degrees": 55.53372222,
+        "E_Degrees": 11.59825556
+    },
+    {
+        "Name": "ST-3",
+        "N_Degrees": 55.53805556,
+        "E_Degrees": 11.605
+    },
+    {
+        "Name": "ST-4",
+        "N_Degrees": 55.51846389,
+        "E_Degrees": 11.58713889
+    },
+    {
+        "Name": "ST-5",
+        "N_Degrees": 55.535625,
+        "E_Degrees": 11.6259
+    },
+    {
+        "Name": "ST-6",
+        "N_Degrees": 55.55798611,
+        "E_Degrees": 11.65236667
+    },
+    {
+        "Name": "ST-7",
+        "N_Degrees": 55.53833333,
+        "E_Degrees": 11.6075
+    },
+    {
+        "Name": "ST-8",
+        "N_Degrees": 55.53859444,
+        "E_Degrees": 11.60717222
+    },
+    {
+        "Name": "ST-9",
+        "N_Degrees": 55.53832778,
+        "E_Degrees": 11.60709444
+    }
+]
