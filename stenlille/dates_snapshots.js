04ef3ca83f60cdfb3c6754f81303c3e09d5513fe
nikolai-andrianov <n_andrianov@hotmail.com>
2023-11-16 01:15:20 +0100

    Plotting snapshots

--- stenlille/dates_snapshots.js
+++ stenlille/dates_snapshots.js
@@ -1,162 +1,162 @@
 const dates_snapshots = [
     {
         "date": "2023-01-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00000.svg"
+        "snapshot": "snapshots/Sg_00000.png"
     },
     {
         "date": "2023-03-02",
-        "snapshot": "./i6Wp6W_BHP/Sg_00001.svg"
+        "snapshot": "snapshots/Sg_00001.png"
     },
     {
         "date": "2023-04-02",
-        "snapshot": "./i6Wp6W_BHP/Sg_00002.svg"
+        "snapshot": "snapshots/Sg_00002.png"
     },
     {
         "date": "2023-05-02",
-        "snapshot": "./i6Wp6W_BHP/Sg_00003.svg"
+        "snapshot": "snapshots/Sg_00003.png"
     },
     {
         "date": "2023-06-02",
-        "snapshot": "./i6Wp6W_BHP/Sg_00004.svg"
+        "snapshot": "snapshots/Sg_00004.png"
     },
     {
         "date": "2023-07-02",
-        "snapshot": "./i6Wp6W_BHP/Sg_00005.svg"
+        "snapshot": "snapshots/Sg_00005.png"
     },
     {
         "date": "2023-08-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00006.svg"
+        "snapshot": "snapshots/Sg_00006.png"
     },
     {
         "date": "2023-09-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00007.svg"
+        "snapshot": "snapshots/Sg_00007.png"
     },
     {
         "date": "2023-10-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00008.svg"
+        "snapshot": "snapshots/Sg_00008.png"
     },
     {
         "date": "2023-11-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00009.svg"
+        "snapshot": "snapshots/Sg_00009.png"
     },
     {
         "date": "2023-12-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00010.svg"
+        "snapshot": "snapshots/Sg_00010.png"
     },
     {
         "date": "2023-12-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00011.svg"
+        "snapshot": "snapshots/Sg_00011.png"
     },
     {
         "date": "2024-01-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00012.svg"
+        "snapshot": "snapshots/Sg_00012.png"
     },
     {
         "date": "2024-03-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00013.svg"
+        "snapshot": "snapshots/Sg_00013.png"
     },
     {
         "date": "2024-04-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00014.svg"
+        "snapshot": "snapshots/Sg_00014.png"
     },
     {
         "date": "2024-05-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00015.svg"
+        "snapshot": "snapshots/Sg_00015.png"
     },
     {
         "date": "2024-05-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00016.svg"
+        "snapshot": "snapshots/Sg_00016.png"
     },
     {
         "date": "2024-07-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00017.svg"
+        "snapshot": "snapshots/Sg_00017.png"
     },
     {
         "date": "2024-07-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00018.svg"
+        "snapshot": "snapshots/Sg_00018.png"
     },
     {
         "date": "2024-08-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00019.svg"
+        "snapshot": "snapshots/Sg_00019.png"
     },
     {
         "date": "2024-09-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00020.svg"
+        "snapshot": "snapshots/Sg_00020.png"
     },
     {
         "date": "2024-10-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00021.svg"
+        "snapshot": "snapshots/Sg_00021.png"
     },
     {
         "date": "2024-11-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00022.svg"
+        "snapshot": "snapshots/Sg_00022.png"
     },
     {
         "date": "2024-12-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00023.svg"
+        "snapshot": "snapshots/Sg_00023.png"
     },
     {
         "date": "2025-01-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00024.svg"
+        "snapshot": "snapshots/Sg_00024.png"
     },
     {
         "date": "2025-03-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00025.svg"
+        "snapshot": "snapshots/Sg_00025.png"
     },
     {
         "date": "2025-04-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00026.svg"
+        "snapshot": "snapshots/Sg_00026.png"
     },
     {
         "date": "2025-05-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00027.svg"
+        "snapshot": "snapshots/Sg_00027.png"
     },
     {
         "date": "2025-05-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00028.svg"
+        "snapshot": "snapshots/Sg_00028.png"
     },
     {
         "date": "2025-07-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00029.svg"
+        "snapshot": "snapshots/Sg_00029.png"
     },
     {
         "date": "2025-07-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00030.svg"
+        "snapshot": "snapshots/Sg_00030.png"
     },
     {
         "date": "2025-08-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00031.svg"
+        "snapshot": "snapshots/Sg_00031.png"
     },
     {
         "date": "2025-09-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00032.svg"
+        "snapshot": "snapshots/Sg_00032.png"
     },
     {
         "date": "2025-10-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00033.svg"
+        "snapshot": "snapshots/Sg_00033.png"
     },
     {
         "date": "2025-11-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00034.svg"
+        "snapshot": "snapshots/Sg_00034.png"
     },
     {
         "date": "2025-12-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00035.svg"
+        "snapshot": "snapshots/Sg_00035.png"
     },
     {
         "date": "2026-01-30",
-        "snapshot": "./i6Wp6W_BHP/Sg_00036.svg"
+        "snapshot": "snapshots/Sg_00036.png"
     },
     {
         "date": "2026-03-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00037.svg"
+        "snapshot": "snapshots/Sg_00037.png"
     },
     {
         "date": "2026-03-31",
-        "snapshot": "./i6Wp6W_BHP/Sg_00038.svg"
+        "snapshot": "snapshots/Sg_00038.png"
     },
     {
         "date": "2026-05-01",
-        "snapshot": "./i6Wp6W_BHP/Sg_00039.svg"
+        "snapshot": "snapshots/Sg_00039.png"
     }
 ] 
