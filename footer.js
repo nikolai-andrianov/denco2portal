5c028dddb5a1355159070b9b4f380c98b12df76d
nikolai-andrianov <n_andrianov@hotmail.com>
2023-11-23 08:44:20 +0100

    Footer

--- footer.js
+++ footer.js
@@ -2,5 +2,8 @@
 
 let footerDiv = document.createElement("div");
 footerDiv.className = "footer";
-footerDiv.innerHTML = `<a href="disclaimer.html">Disclaimer</a> &nbsp; &#9679; &nbsp; &copy; CO<sub>2</sub> Portal 2023 &nbsp; &#9679; &nbsp; Last updated Nov 22`;
+footerDiv.innerHTML = `
+<!-- <a href="disclaimer.html">Disclaimer</a> &nbsp; &#9679; &nbsp; &copy; CO<sub>2</sub> Portal 2023 &nbsp; &#9679; &nbsp; Last updated Nov 22 -->
+<a href="disclaimer.html">Disclaimer</a> &#9679; &copy; CO<sub>2</sub> Portal 2023 &#9679; Last updated Nov 23 
+`;
 document.body.appendChild(footerDiv);
