26688bff7ce2d390eb7a528d05e5f33dad3d639a
nikolai-andrianov <n_andrianov@hotmail.com>
2023-11-23 15:22:42 +0100

    Scale & about

--- footer.js
+++ footer.js
@@ -4,6 +4,6 @@ let footerDiv = document.createElement("div");
 footerDiv.className = "footer";
 footerDiv.innerHTML = `
 <!-- <a href="disclaimer.html">Disclaimer</a> &nbsp; &#9679; &nbsp; &copy; CO<sub>2</sub> Portal 2023 &nbsp; &#9679; &nbsp; Last updated Nov 22 -->
-<a href="disclaimer.html">Disclaimer</a> &#9679; &copy; CO<sub>2</sub> Portal 2023 &#9679; Last updated Nov 23 
+&copy; CO<sub>2</sub> Portal 2023 &nbsp; &#9679; &nbsp; Last updated Nov 23
 `;
 document.body.appendChild(footerDiv);
